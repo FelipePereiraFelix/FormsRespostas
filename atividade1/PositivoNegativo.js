630f8291677fe776a2ea40ed89eb15fac7479fdf
Felix <88720299+FelipePereiraFelix@users.noreply.github.com>
2021-09-21 18:26:29 -0300

    Create PositivoNegativo.js

---
+++ atividade1/PositivoNegativo.js
@@ -0,0 +1,9 @@
+var valor = parseInt(prompt("Informe um valor"));
+if(valor == 0) {
+document.write("Neutro");
+}
+if(valor>0){
+document.write("Positivo");
+
+}else if (valor<0) { 
+document.write("Negativo");
