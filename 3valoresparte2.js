38b74ecec930d6fa2c9fd600fecb372099a15390
Felix♨ <88720299+FelipePereiraFelix@users.noreply.github.com>
2021-09-22 11:10:10 -0300

    Add files via upload

---
+++ 3valoresparte2.js
@@ -0,0 +1,18 @@
+var num1 = parseInt(prompt("Informe o primeiro valor"));
+var num2 = parseInt(prompt("Informe mais um valor"));
+var num3 = parseInt(prompt("Só mais umzinho"));
+var valor = num1 + num2 || num3 + num2 || num1 + num3;
+
+
+if(num2> num1 && num3> num1)
+ num3 + num2;
+ if(num3> num2 && num1>num2)
+ num3 + num1;
+else if(num2> num3 && num1>num3)
+ num2 + num1;
+
+ document.write("A soma dos 2 maiores valores é " + valor);
+ 
+
+ 
+
