4654325275802090beaadbee8761db24f545c050
Felix♨ <88720299+FelipePereiraFelix@users.noreply.github.com>
2021-09-22 11:22:02 -0300

    Add files via upload

---
+++ atividade6/Forms.js
@@ -0,0 +1,20 @@
+var num1 = parseInt(prompt("Informe o primeiro valor"));
+var num2 = parseInt(prompt("Informe mais um valor"));
+ 
+if(num1> num2){
+document.write("O primeiro valor é maior");
+}
+else if(num2> num1){
+document.write("O segundo valor é o maior");
+}
+else {
+document.write("Valores iguais");
+}
+
+
+
+
+ 
+
+ 
+
