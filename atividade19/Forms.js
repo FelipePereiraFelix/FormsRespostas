e90bd7bd8a51f652d06547633b090fd991b4db9e
Felix♨ <88720299+FelipePereiraFelix@users.noreply.github.com>
2021-09-29 20:19:55 -0300

    Add files via upload

---
+++ atividade19/Forms.js
@@ -0,0 +1,18 @@
+var a = parseInt(prompt("Digite o valor numerico da letra A:"));
+var b = parseInt(prompt("Digite o valor numerico da letra B:"));
+var troca;
+
+if(troca = a){
+
+}
+else if(a = b){
+
+}
+else if(b = troca){
+
+}
+document.write("O novo valor de B é : " + troca );
+document.write("O novo valor de A é : " + b );
+
+
+
